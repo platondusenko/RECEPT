9d68aa6b7a9bd8952ec0efb3cb95b68c628bd2c1
Arseniiy608 <arseniiybryazkalo@gmail.com>
2026-09-20 19:06:37 +0300

    ingradients

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function getRecipe() {
   document.getElementById('recept').innerHTML = `
  
     <h2 style="color: #2e39b4; text-align: center; margin-bottom: 30px;">${recipe.strMeal}</h2>
-    <img style="border-radius: 12px; float: left; margin-right: 20px; height: 300px; width: 400px; box-shadow: 0 2px 4px rgba(79, 70, 229, 0.2);" src="${recipe.strMealThumb}" width="300" height="200" alt="${recipe.strMeal}">
+    <img style="border-radius: 12px;  margin-right: 20px; height: 300px; float: left; width: 400px; box-shadow: 0 2px 4px rgba(79, 70, 229, 0.2);" src="${recipe.strMealThumb}" width="300" height="200" alt="${recipe.strMeal}">
     <ul style="color: #0F4C81; margin-left: 20px; float: left;  margin-top: 5px; ">
       ${Array.from({ length: 20 }, (_, i) => {
         const ingredient = recipe[`strIngredient${i + 1}`];
@@ -15,8 +15,8 @@ async function getRecipe() {
         return '';
       }).join('')}
     </ul>
-          <p style="color: #0F4C81; margin-top: 20px; padding-left: 50px; float: right;  "> <h4 > 👨‍🍳 Поради щодо приготування </h4>
+          <p style="color: #0F4C81; margin-top: 20px; padding-left: 50px; float: left;  "> <h4 > 👨‍🍳 Поради щодо приготування </h4>
  🧼 Підготуйте всі необхідні інгредієнти.  <br 🥣 Підготуйте посуд та кухонне приладдя.  <br>🔪 Наріжте або підготуйте продукти за потреби.  <br>🍳 Готуйте страву, дотримуючись рецепта.  <br>🧂 Дodайте спеції за власним смаком.  <br>🍽️ Подавайте страву після завершення приготування.</p>
- 
+
 `;
 }
